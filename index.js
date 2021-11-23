3edecf18656ae2579649da05d92daee355012a4e
mtlankenau <mtlankenau@gmail.com>
2021-11-23 17:26:08 -0500

    added inquirer prompt and database query for adding new departments to database

--- index.js
+++ index.js
@@ -27,15 +27,36 @@ const initialOptions = () => {
   ])
 };
 
+const addDepartmentPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: 'text',
+      name: 'newDepartment',
+      message: 'Enter the name of the new department.',
+      validate: nameInput => {
+        if (nameInput) {
+          return true
+        } else {
+          console.log('Please enter the new department name!')
+          return false;
+        }
+      }
+    }
+  ])
+}
+
 const handleChoice = () => {
   initialOptions()
     .then(selection => {
       if (selection.choice === 'View all departments') {
-        return viewDepartments();
+        viewDepartments();
       } else if (selection.choice === 'View all roles') {
-        return viewRoles();
+        viewRoles();
       } else if (selection.choice === 'View all employees') {
-        return viewEmployees();
+        viewEmployees();
+      } else if (selection.choice === 'Add a department') {
+        addDepartmentPrompt()
+          .then(selection => addNewDepartment(selection.newDepartment))
       }
     })
 };
@@ -80,5 +101,22 @@ const viewEmployees = () => {
   .catch(console.log)
   .then(handleChoice);
 };
+
+const addNewDepartment = (department) => {
+  const sql = `INSERT INTO department (name)
+              VALUES ('${department}')`;
+
+  db.promise().query(sql)
+    .then( ([rows,fields]) => {
+      console.log(
+`===========================================================
+
+The ${department} department has been added to the database!
+
+===========================================================`)
+    })
+    .catch(console.log)
+    .then(handleChoice);
+}
 
 handleChoice();
