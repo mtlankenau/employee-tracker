8394a3e1fd2d39025aa44a7a474702e9955b423e
mtlankenau <mtlankenau@gmail.com>
2021-12-13 15:49:23 -0500

    removed unnecessary code

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const inquirer = require('inquirer');
-const express = require('express');
 const db = require('./db/connection');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const initialOptions = () => {
   return inquirer.prompt([
